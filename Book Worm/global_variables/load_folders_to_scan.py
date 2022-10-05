eaea5d54c935bbc7f47dcfa6bb29031abe46af5c
Antonio89743 <psnkomarec@gmail.com>
2022-10-05 17:55:03 +0200

    Go

--- Book Worm/global_variables/load_folders_to_scan.py
+++ Book Worm/global_variables/load_folders_to_scan.py
@@ -3,7 +3,7 @@ import glob, os
 import json
 
 def load_folders_to_scan():
-    save_folder_path = r"C:\Users\anton\AppData\Roaming\BookWorm"
+    save_folder_path = r"C:\Users\anton\AppData\Roaming\BookWorm" #this will have to be relative
     folders_to_scan_array :array = []
     file = open(os.path.join(save_folder_path, 'folders_to_scan.json'), 'r')
     json_file_data = file.read()
